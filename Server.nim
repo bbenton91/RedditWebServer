814267fed4f3ccdf7e60dc057111f7ef1d121207
Brad <bbenton91@outlook.com>
2021-01-22 15:14:19 -0500

    Added super basic sessions

--- Server.nim
+++ Server.nim
@@ -1,28 +1,45 @@
-import asynchttpserver, asyncdispatch, nim-templates-master/templates, RedditApi/Reddit, json
+import asynchttpserver, asyncdispatch, nim-templates-master/templates, json, tables
+import RedditApi/Reddit, Session
 
+let session = newSession()
 var server = newAsyncHttpServer()
 
 proc testPage (name: string, results: seq[JsonNode]): string = tmplf "test.html"
 
 # echo testPage("Charlie")
 
-proc cb(req: Request) {.async.} =
+proc cb(req: Request) {.async, gcsafe.} =
     if req.url.path == "/get-saved" and req.reqMethod == HttpMethod.HttpPost:
         echo "got a post"
-        echo req.body
+        # echo req.body
+        # echo req.headers
 
-        let code = parseJson(req.body)["code"].getStr()
+        # Get the post data
+        let postData = parseJson(req.body)
+        let webToken = postData["token"].getStr("")
+        let sessionId = postData["sessionId"].getStr("")
 
-        # var reddit = newReddit("y_LmR31bMjlTBQ", "Cnzt0PoMNEh2YztgC_JvQVg5En7oBA", "Pahaz", "Suchasandorwith12")
-        # Connect using web auth
-        var user = Reddit.connectAuth("C6iDiQaoPTwgVw","m9Ze8qmjzHO7yuU9KyE3lCKoJQzdYQ", code, "http://localhost:3000")
+        # Grab our session
+        var (userSession, newSessionId) = session.getUserSession(sessionId, "")
+
+        # Then we can either make a new user or grab one for the session
+        var user:User
+        if userSession == nil or userSession.user.kind == InvalidUser: # If the session is nil or our user is invalid, we need a new user
+            echo "session is nil or user kind is invalid. Nil? " & $(userSession == nil)
+            # Connect using web auth
+            user = Reddit.connectAuth("C6iDiQaoPTwgVw","m9Ze8qmjzHO7yuU9KyE3lCKoJQzdYQ", webToken, "http://localhost:3000")
+        else: # Otherwise we got one right here
+            user = userSession.user
+
         # If the user is valid, try to get the saved posts. Otherwise, return default data
         let (saved, newUser) = if user.kind == ValidUser:
                                     user.getSaved(fields = @["title"])
                                 else:
                                     (newSeq[JsonNode](), user)
 
-        echo saved.len
+        # Then we save back into the session. Note here that newUser could be an InvalidUser. The next request for the session will
+        # have to authenticate with reddit again
+        session.setUserSession(newSessionId, sessionId, "", "", newUserSession(newUser))
 
         let headers = newHttpHeaders()
         headers["Access-Control-Allow-Headers"] = "Content-Type,access-control-allow-origin"
@@ -30,12 +47,17 @@ proc cb(req: Request) {.async.} =
         headers["Access-Control-Allow-Methods"] = "POST,GET"
         headers["Content-Type"] = "application/json"
 
-        # This is our data to return to the server. For the token, we use the newUser.token if its a valid user. Otherwise just empty string
-        let returnData = {"data": $saved, "token": if newUser.kind == ValidUser: newUser.token else: ""}
+        echo "Sending new session id " & newSessionId
 
-        await req.respond(Http200, $saved, headers)
+        # This is our data to return to the server. For the token, we use the newUser.token if its a valid user. Otherwise just empty string
+        let returnData = %* {"data": saved, 
+                            "sessionId": if newUser.kind == ValidUser: newSessionId else: ""}
+
+        await req.respond(Http200, $ returnData, headers)
 
     elif req.url.path == "/list" and req.reqMethod == HttpMethod.HttpPost:
+        # var user = connectPassword("aI-ujHrrKdOsZg", "dNw2jGHX65ejS8JxkMDkKZtNKk8", "Pahaz", "Suchasandorwith12")
+
         var user = connectPassword("y_LmR31bMjlTBQ", "Cnzt0PoMNEh2YztgC_JvQVg5En7oBA", "Pahaz", "Suchasandorwith12")
         var (data, newUser) = user.getSaved(fields = @["title"])
         
---
+++ Session.nim
@@ -0,0 +1,43 @@
+import tables, RedditApi/Reddit, random
+
+type 
+    UserSession = ref object
+        user*: User
+
+    Session = ref object
+        sessionTable: TableRef[string, UserSession]
+
+proc newSession*():Session =
+    Session(sessionTable: newTable[string, UserSession]())
+
+proc newUserSession*(user:User):UserSession =
+    UserSession(user:user)
+
+method getNewSessionId(this:Session, length:int):string {.base.} =
+    # @ through Z is 64-90
+    # a through z is 96-122
+    # We need to essentially add anything higher than 90 by 6 to exclude the symbols
+
+    randomize()
+    var id:string = ""
+    var counter = 0
+    while counter < length:
+        var num = rand(64..(122-6)) # We subtract the higher end by 6 because we add 6 below to raise it up. This is to exclude a section of ascii symbols
+        num = if num > 90: num + 6 else: num
+        id &= $char(num) # Convert the ascii number to a character. Then $ converts to string
+        counter += 1
+    id
+
+method validateSession(this:Session) {.base.} =
+    discard
+
+method getUserSession*(this:Session, sessionId:string, sessionKey:string):(UserSession, string) {.base.} =
+    # TODO need to validate here before getting
+    #validate
+    ( this.sessionTable.getOrDefault(sessionId, nil), this.getNewSessionId(24) )
+
+method setUserSession*(this:Session, sessionId:string, oldSessionId:string, newSessionKey:string, oldSessionKey:string, userSession: UserSession) {.base.} =
+    this.sessionTable[sessionId] = userSession
+    this.sessionTable.del(oldSessionId)
+    # TODO probably should validate here before setting
+
