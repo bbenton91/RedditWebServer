5a75a557f472c87ea01cb4e7a7c4ae098a4874c7
Brad <bbenton91@gmail.com>
2021-02-22 23:17:48 -0500

    Implemented async and expiration for tokens

--- Server.nim
+++ Server.nim
@@ -1,5 +1,5 @@
 import asynchttpserver, asyncdispatch, nim-templates-master/templates, json, tables
-import RedditApi/Reddit, Session
+import RedditApi/src/Reddit, Session
 
 let session = newSession()
 var server = newAsyncHttpServer()
@@ -16,15 +16,30 @@ proc headers():HttpHeaders =
         "Content-Type":"application/json"
     })
 
+proc getChunkedContent(user:User, isNew:bool, after:string): Future[(JsonNode, seq[JsonNode], User)] {.async.} =
+    ## Gets partial saved content. `user` is the User to pull content from, `isNew` is to signal if this is a brand-new request,
+    ## and `after` is a string which is the last 'after' object from a reddit api request.
+    
+    # If 'after' is empty, then we can assume we don't need to fetch anymore
+    if after == "":
+        result = (newJObject(), newSeq[JsonNode](), user)
+    else:
+        let urlParams = newUrlParams({"after": after}.toTable());
+
+        let (rawJson, savedLinks, userAfterLinks) = if user.kind == ValidUser:
+                                    await user.getSaved(fields = @["title", "url", "permalink", "id", "is_self", "subreddit", "author"], extraParams=urlParams)
+                                else:
+                                    (newJObject(), newSeq[JsonNode](), user)
+
 proc fetchAndReturnSavedListings(req:Request, user:User, webToken:string, newSessionId:string, oldSessionId:string) {.async.} =
     # If the user is valid, try to get the saved posts. Otherwise, return default data
-    let (savedLinks, userAfterLinks) = if user.kind == ValidUser:
-                                user.getAllSaved(fields = @["title", "url", "permalink", "id", "is_self", "subreddit", "author"])
+    let (_, savedLinks, userAfterLinks) = if user.kind == ValidUser:
+                                await user.getAllSaved(fields = @["title", "url", "permalink", "id", "is_self", "subreddit", "author"])
                             else:
                                 (newSeq[JsonNode](), user)
 
     let (savedComments, refreshedUser) = if userAfterLinks.kind == ValidUser:
-                                userAfterLinks.getAllSaved(fields = @["title", "url", "permalink", "id", "is_self", "subreddit", "author", "body"], listingType = Comment)
+                                await userAfterLinks.getAllSaved(fields = @["title", "url", "permalink", "id", "is_self", "subreddit", "author", "body"], listingType = Comment)
                             else:
                                 (newSeq[JsonNode](), userAfterLinks)
 
@@ -45,7 +60,7 @@ proc unsaveListing(req:Request, user:User, webToken:string, newSessionId:string,
     let fullname = postData["fullname"].getStr("")
 
     let (sucess, newUser) = if user.kind == ValidUser:
-                                user.unsave(fullname)
+                                await user.unsave(fullname)
                             else:
                                 (false, user)
     
@@ -79,65 +94,22 @@ proc cb(req: Request) {.async, gcsafe.} =
         if userSession == nil or userSession.user.kind == InvalidUser: # If the session is nil or our user is invalid, we need a new user
             echo "session is nil or user kind is invalid. Nil? " & $(userSession == nil)
             # Connect using web auth
-            user = Reddit.connectAuth("C6iDiQaoPTwgVw","m9Ze8qmjzHO7yuU9KyE3lCKoJQzdYQ", webToken, "http://localhost:3000")
+            user = await Reddit.connectAuth("C6iDiQaoPTwgVw","m9Ze8qmjzHO7yuU9KyE3lCKoJQzdYQ", webToken, "http://localhost:3000")
         else: # Otherwise we got one right here
             user = userSession.user
+            if user.tokens.isExpired():
+                user = newInvalidUser("Session expired")
 
         if req.url.path == "/get-saved":
             await fetchAndReturnSavedListings(req, user, webToken, newSessionId, sessionId)
         elif req.url.path == "/unsave":
             await unsaveListing(req, user, webToken, newSessionId, sessionId)
 
-
-    # if req.url.path == "/get-saved" and req.reqMethod == HttpMethod.HttpPost:
-    #     echo "got a post"
-    #     # echo req.body
-    #     # echo req.headers
-
-    #     # Get the post data
-    #     let postData = parseJson(req.body)
-    #     let webToken = postData["token"].getStr("")
-    #     let sessionId = postData["sessionId"].getStr("")
-
-    #     # Grab our session
-    #     var (userSession, newSessionId) = session.getUserSession(sessionId, "")
-
-    #     # Then we can either make a new user or grab one for the session
-    #     var user:User
-    #     if userSession == nil or userSession.user.kind == InvalidUser: # If the session is nil or our user is invalid, we need a new user
-    #         echo "session is nil or user kind is invalid. Nil? " & $(userSession == nil)
-    #         # Connect using web auth
-    #         user = Reddit.connectAuth("C6iDiQaoPTwgVw","m9Ze8qmjzHO7yuU9KyE3lCKoJQzdYQ", webToken, "http://localhost:3000")
-    #     else: # Otherwise we got one right here
-    #         user = userSession.user
-
-    #     # If the user is valid, try to get the saved posts. Otherwise, return default data
-    #     let (saved, newUser) = if user.kind == ValidUser:
-    #                                 user.getSaved(fields = @["title", "url", "permalink", "id", "is_self"])
-    #                             else:
-    #                                 (newSeq[JsonNode](), user)
-
-    #     # Then we save back into the session. Note here that newUser could be an InvalidUser. The next request for the session will
-    #     # have to authenticate with reddit again
-    #     session.setUserSession(newSessionId, sessionId, "", "", newUserSession(newUser))
-
-    #     let headers = newHttpHeaders()
-    #     headers["Access-Control-Allow-Headers"] = "Content-Type,access-control-allow-origin"
-    #     headers["Access-Control-Allow-Origin"] = "*"
-    #     headers["Access-Control-Allow-Methods"] = "POST,GET"
-    #     headers["Content-Type"] = "application/json"
-
-    #     # This is our data to return to the server. For the token, we use the newUser.token if its a valid user. Otherwise just empty string
-    #     let returnData = %* {"data": saved, 
-    #                         "sessionId": if newUser.kind == ValidUser: newSessionId else: ""}
-
-    #     await req.respond(Http200, $ returnData, headers)
-
     elif req.url.path == "/list" and req.reqMethod == HttpMethod.HttpPost:
         # var user = connectPassword("aI-ujHrrKdOsZg", "dNw2jGHX65ejS8JxkMDkKZtNKk8", "Pahaz", "Suchasandorwith12")
 
         var user = connectPassword("y_LmR31bMjlTBQ", "Cnzt0PoMNEh2YztgC_JvQVg5En7oBA", "Pahaz", "Suchasandorwith12")
-        var (data, newUser) = user.getSaved(fields = @["title"])
+        var (data, newUser) = await user.getSaved(fields = @["title"])
         
         echo "listing"
         await req.respond(Http200, testPage("Paha", data))
