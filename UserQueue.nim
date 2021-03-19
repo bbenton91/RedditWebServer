6e494aef368781e14471d337563b85f853374657
Brad <bbenton91@gmail.com>
2021-03-19 14:31:29 -0400

    Expanded and refined the UserQueue and UserTable types

--- UserQueue.nim
+++ UserQueue.nim
@@ -1,37 +1,55 @@
-import tables, sugar, options, heapqueue
+import tables, sugar, options, heapqueue, asyncdispatch
 import ../RedditApi/src/Reddit
 
 type
-    Action = ref object
-        fun*: proc()
+    Action* = ref object
+        fun*: proc():Future[void] {.async, gcsafe.}
+
+    UserTable* = ref object
+        table: TableRef[string, UserQueue]
 
     UserQueue* = ref object
-        isProcessing: bool
+        isProcessing*: bool
         queue: HeapQueue[Action]
         # table: TableRef[string, HeapQueue[Action]]
 
-proc newAction*(fun: proc()): Action =
+proc newAction*(fun: proc():Future[void] {.gcsafe.}): Action =
     Action(fun: fun)
 
-proc newQueueTable*():TableRef[string, UserQueue] =
-    let table = newTable[string, UserQueue]()
-    table
+proc newQueueTable*():UserTable =
+    UserTable(table: newTable[string, UserQueue]())
 
 proc newUserQueue(): UserQueue =
     UserQueue(queue: initHeapQueue[Action](), isProcessing: false)
 
-method popNextAction*(this: TableRef[string, UserQueue], name:string): Option[Action] {.base.} =
-    if this.contains(name) and this[name].queue.len > 0:
-        let val = some(this[name].queue.pop())
-        if this[name].queue.len == 0: # make sure to clean up empty queues
-            this.del(name)
-        return val
+proc `[]`*(this: UserTable, name:string): Option[UserQueue] =
+    if this.table.contains(name):
+        return some(this.table[name])
+    none(UserQueue)
 
+proc `[]=`*(this: UserTable, name:string, value:UserQueue) =
+    this.table[name] = value
+
+method contains*(this:UserTable, name:string):bool {.base.} = 
+    this.table.contains(name)
+
+iterator values*(this:UserTable):UserQueue =
+    for item in this.table.values:
+        yield item
+
+method addNewUser*(this:UserTable, name:string) {.base.} =
+    this.table[name] = newUserQueue()
+
+method popNextAction*(this: UserQueue): Option[Action] {.base.} =
+    if this.queue.len > 0:
+        let val = some(this.queue.pop())
+        # if this.queue.len == 0: # make sure to clean up empty queues
+        #     this.table.del(name)
+        return val
     return none(Action)
 
-method pushAction*(this:TableRef[string, UserQueue], name:string, action:Action) {.base.} =
-    if this.contains(name):
-        this[name].queue.push(action)
-    else:
-        this[name] = newUserQueue()
-        this[name].queue.push(action)
+method pushAction*(this:UserQueue, action:Action) {.base.} =
+    this.queue.push(action)
+    
+method hasNextAction*(this:UserQueue):bool {.base.} =
+    this.queue.len > 0
