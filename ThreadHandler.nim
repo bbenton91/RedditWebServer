8e3306f4f026a87e49fdfae58b1107c6e16ebff3
Brad <bbenton91@gmail.com>
2021-03-27 16:59:20 -0400

    Reworked the thread

--- ThreadHandler.nim
+++ ThreadHandler.nim
@@ -1,48 +1,54 @@
-import asyncdispatch, options
-import UserQueue, SharedChannel
+import asyncdispatch, options, os
+import UserQueue
+import Message
 
-type
-    Message* = ref object
-        action*:proc():Future[void]{.async, gcsafe.}
-        userName*: string
+# type
+#     Message* = ref object
+#         action*:proc():Future[void]{.async, gcsafe.}
+#         username*: string
 
-# var chan*: Channel[Message]
+var chan*: Channel[ChannelMessage]
 
 proc processAction*(fun:proc(){.async, gcsafe.}, userQueue:UserQueue):Future[void] {.async, gcsafe.} =
     userQueue.isProcessing = true
     await fun()
     userQueue.isProcessing = false
 
-proc receiver*(queueTable: UserTable) {.thread.} =
+proc receiver*() {.thread.} =
     echo "Receiver is starting"
-    # let queueTable = newQueueTable()
+    let queueTable = newQueueTable()
     while true:
-        # let data = tryRecv(channel[])
+        let data = chan.tryRecv()
+        if data.dataAvailable:
+            echo "Got it"
 
         # If we had a message, let's process it
-        # if data.dataAvailable:
-        #     echo "Got msg"
-        #     var userQueueOption = queueTable[data.msg.userName]
+        if data.dataAvailable:
+            echo "Got msg"
+            var userQueueOption = queueTable[data.msg.username]
 
-        #     # If we don't have a userQueue, make an entry first
-        #     if userQueueOption.isNone:
-        #         queueTable.addNewUser(data.msg.userName)
-        #         userQueueOption = queueTable[data.msg.userName] # Reassign
+            # If we don't have a userQueue, make an entry first
+            if userQueueOption.isNone:
+                queueTable.addNewUser(data.msg.username)
+                userQueueOption = queueTable[data.msg.username] # Reassign
            
-        #     let userQueue = userQueueOption.get
+            let userQueue = userQueueOption.get
 
-        #     # If the queue is busy or has something in line
-        #     if userQueue.isProcessing or userQueue.hasNextAction:
-        #         userQueue.pushAction newAction(data.msg.action)
-        #     # Otherwise we can just do it immediately
-        #     else:
-        #         waitFor processAction(data.msg.action, userQueue)
+            # If the queue is busy or has something in line
+            if userQueue.isProcessing or userQueue.hasNextAction:
+                # userQueue.pushAction newAction(data.msg.action)
+                discard
+            # Otherwise we can just do it immediately
+            else:
+                discard
+                # waitFor processAction(data.msg.action, userQueue)
 
-        # # If no message, lets keep working on the queue if possible
-        # else:
-        for queue in queueTable.values: 
-            # If the queue is not busy but has something left to process
-            if not queue.isProcessing and queue.hasNextAction():
-                let action = queue.popNextAction()
-                if action.isSome:
-                    waitFor processAction(action.get.fun, queue)
+        # If no message, lets keep working on the queue if possible
+        else:
+            for queue in queueTable.values: 
+                # If the queue is not busy but has something left to process
+                if not queue.isProcessing and queue.hasNextAction():
+                    let action = queue.popNextAction()
+                    if action.isSome:
+                        discard
+                        # asyncCheck processAction(action.get.fun, queue)
